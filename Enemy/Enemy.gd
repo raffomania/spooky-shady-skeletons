6b3b332a1df8cf34732642cc24c09cb6b789fc3b
Karim Djemai <karimdjemai1997@gmail.com>
2023-10-07 01:04:42 +0200

    instantiate enemy as plane rotated towards cam

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,14 @@
-extends Node3D
+extends StaticBody3D
 class_name Enemy
+
+# @export
+# var camera : Camera3D
 
 func _ready():
 	add_to_group("enemies")
+	look_at(-get_viewport().get_camera_3d().position, Vector3.RIGHT, false)
+	rotate_object_local(Vector3.FORWARD, PI / 3.49)
+	# rotate_x(PI / 2)
+
+# func _process(delta):
+	#turn to camera
