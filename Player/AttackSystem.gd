727942cf4513e5ba780b4318496e5c7f0ce22968
Karim Djemai <karimdjemai1997@gmail.com>
2023-10-07 01:04:02 +0200

    initialize attack system

---
+++ Player/AttackSystem.gd
@@ -0,0 +1,18 @@
+extends Area3D
+
+@export
+var damage : int # percent damage per attack
+
+var attackable_enemies : Array[Enemy]
+
+
+func enemy_entered(node: Node3D):
+	#check if node is enemy
+	if (node is Enemy):
+		attackable_enemies.append(node)
+		print_debug(node.name)
+
+
+
+func _ready():
+	body_entered.connect(enemy_entered)
