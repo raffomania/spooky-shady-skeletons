f623d26cef350084dfafcf49bc61b3d3964ff321
HarkMerkau <harkmerkau4@gmail.com>
2023-10-07 03:00:54 +0200

    shiity enemy follow player

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,9 +1,16 @@
-extends StaticBody3D
+extends CharacterBody3D
 class_name Enemy
+#movement noch vo player geschwindigkeit abhängig machen
+@export var movement_speed= 10.0
 
-# @export
-# var camera : Camera3D
+# var camera : Camera3
+@onready var player = get_tree().get_first_node_in_group("player")
 
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+	
 func _ready():
 	add_to_group("enemies")
 	look_at(-get_viewport().get_camera_3d().position, Vector3.RIGHT, false)
@@ -12,3 +19,5 @@ func _ready():
 
 # func _process(delta):
 	#turn to camera
+	
+	
