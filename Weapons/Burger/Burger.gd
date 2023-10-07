99a5745d44f26c667248ae58d6f17a276ce0305e
C-gotoh <shigotoh1@gmail.com>
2023-10-07 17:20:52 +0200

    burger: modify speed multiplier on kick
    Signed-off-by: C-gotoh <shigotoh1@gmail.com>

--- Weapons/Burger/Burger.gd
+++ Weapons/Burger/Burger.gd
@@ -7,7 +7,11 @@ var timer: Timer
 
 var movement: Vector3 = Vector3.LEFT.rotated(Vector3.UP, PI / 4)
 
-var speed: float = 5.0
+var speed: float = 2.0
+
+var speed_multiplier: float = 1.0
+
+var max_speed_multiplier: float = 4.0
 
 func _ready():
     area_entered.connect(on_area_entered)
@@ -20,7 +24,7 @@ func _ready():
 
 
 func _process(delta):
-    position += delta * movement * speed
+    position += delta * movement * speed * speed_multiplier
     $burger.rotate_y(delta * 4)
 
 
@@ -36,5 +40,8 @@ func reverse():
         var player_movement = $"../".movement
         if (player_movement != Vector3.ZERO):
             movement = player_movement.normalized()
+            speed_multiplier = min(max_speed_multiplier, speed_multiplier + 1.0)
+        else:
+            speed_multiplier = 1.0
     else:
         movement = movement.rotated(Vector3.UP, PI)
