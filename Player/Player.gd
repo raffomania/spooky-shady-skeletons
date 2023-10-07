3d4c9e229294361b9a73e0bebd0c4b116b9de2c0
Rafael Epplée <hello@rafa.ee>
2023-10-07 10:34:52 +0200

    Animate player movements

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,11 @@ var dash_speed_multiplier: float = 4
 
 # Dash duration in seconds
 @export
-var dash_duration: float = 0.5
+var dash_duration: float = 0.25
+
+# Base movement speed
+@export
+var movement_speed: float = 2
 
 var dashing = false
 var movement = Vector3.ZERO
@@ -16,10 +20,14 @@ var current_dash_duration = 0
 var health_percent: float:
 	set = set_health
 
+var animation_player: AnimationPlayer
+
 func _ready():
 	# trigger set_health method
 	health_percent = 100.0
 	$DamageDetector.area_entered.connect(attacked_by_enemy)
+	animation_player = $Model/AnimationPlayer
+	animation_player.get_animation("idle").loop_mode = Animation.LOOP_LINEAR
 
 func _process(delta: float):
 	if (current_dash_duration >= dash_duration):
@@ -31,7 +39,7 @@ func _process(delta: float):
 	else:
 		var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 		var z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-		movement = Vector3(x, 0, z).rotated(Vector3.UP, PI / 4).normalized() * delta
+		movement = Vector3(x, 0, z).rotated(Vector3.UP, PI / 4).normalized() * delta * movement_speed
 
 		if (Input.get_action_strength("dash") and movement != Vector3.ZERO):
 			movement *= dash_speed_multiplier
@@ -41,6 +49,15 @@ func _process(delta: float):
 	
 	if (movement.length() > 0):
 		$'Model'.look_at(transform.origin + movement, Vector3.UP, true) 
+
+	# Select animation that should be playing
+	if dashing:
+		animation_player.play("crouch")
+	elif movement != Vector3.ZERO:
+		animation_player.play("sprint")
+	else:
+		animation_player.play("idle")
+
 
 func attacked_by_enemy(other: Area3D):
 	if other.is_in_group("enemies"):
