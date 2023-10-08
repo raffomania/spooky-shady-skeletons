11ecfc5be459ed4894a10ff2f66c40f804f07127
Karim Djemai <karimdjemai1997@gmail.com>
2023-10-08 16:10:41 +0200

    initialize bat enemy

---
+++ Enemy/Bat/Bat.gd
@@ -0,0 +1,14 @@
+extends Enemy
+class_name PumpkinMinion
+
+func _ready():
+    super()
+    health = 50
+    movement_speed = 1.7
+    xp = 2.0
+    GlobalClock.beat.connect(jump_towards_player)
+
+
+func _process(delta):
+    move_towards_player(delta)
+    set_animation_shader_param()
