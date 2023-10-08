ebeef9304520d3c8a5f9817f2fd3fac224b8c394
C-gotoh <shigotoh1@gmail.com>
2023-10-08 16:48:10 +0200

    pumpkin: nerve minion speed
    Signed-off-by: C-gotoh <shigotoh1@gmail.com>

--- Enemy/PumpkinMinion/PumpkinMinion.gd
+++ Enemy/PumpkinMinion/PumpkinMinion.gd
@@ -4,7 +4,7 @@ class_name PumpkinMinion
 func _ready():
     super()
     health = 150
-    movement_speed = 0.8
+    movement_speed = 0.5
     xp = 2.0
     GlobalClock.beat.connect(jump_towards_player)
 
